4e07c7acb62f6ef2eca3b9e298a939826895162a
Daniel Alesanco <dalesanco@gmail.com>
2019-10-18 17:42:17 +0200

    Initial Commit

---
+++ DictadoLengua/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DictadoLengua
+//
+//  Created by Daniel Alesanco on 18/10/2019.
+//  Copyright © 2019 Daniel Alesanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
