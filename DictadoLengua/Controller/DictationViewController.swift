a718bbe7bf8ef808ad38e7d5fd836b1f0035c7a8
Daniel Alesanco <dalesanco@gmail.com>
2019-10-23 18:31:57 +0200

    Solves issues on Dictation View Controller - shows keyboard when a word is tapped - shows text field just over keyboard - play click sound when OK button on text field is presses

--- DictadoLengua/Controller/DictationViewController.swift
+++ DictadoLengua/Controller/DictationViewController.swift
@@ -7,128 +7,108 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class DictationViewController: UIViewController, UITextFieldDelegate {
 
-    
+    //------------------------------------------------------
+    // MARK: - Outlet references
+
     @IBOutlet var dictadoTextView: UITextView!
     @IBOutlet var whiteBackground: UIImageView!
     @IBOutlet var correctedWordTextField: UITextField!
     @IBOutlet var correctedWordUIView: UIView!
     @IBOutlet var correctedWordHeightConstraint:  NSLayoutConstraint!
 
-    let dictado = "Todas las tardes, a la salida de la escuela, los niños se habían acostumbrado a ir a jugar al jardín del gigante. Era un jardín grande y hermoso, cubierto de verde y suave césped. Dispersas sobre la hierba brillaban bellas flores como estrellas, y había una docena de melocotones que, en primavera, se cubrían de delicados capullos rosados, y en otoño daban sabroso fruto. \n\nLos pájaros se posaban en los árboles y cantaban tan deliciosamente que los niños interrumpían sus juegos para escucharlos."
+    //------------------------------------------------------
+    // MARK: - Global Variables
+
+    private let originalText = "Todas las tardes, a la salida de la escuela, los niños se habían acostumbrado a ir a jugar al jardín del gigante. Era un jardín grande y hermoso, cubierto de verde y suave césped. Dispersas sobre la hierba brillaban bellas flores como estrellas, y había una docena de melocotones que, en primavera, se cubrían de delicados capullos rosados, y en otoño daban sabroso fruto. \n\nLos pájaros se posaban en los árboles y cantaban tan deliciosamente que los niños interrumpían sus juegos para escucharlos."
     
-    var tildeDictation = false
-    var bvDictation = false
-    var llyDictation = false
-    var gjDictation = false
-    var hDictation = false
+    //------------------------------------------------------
+    // MARK: - Variables controlled by home View Controller
+
+    var tildeDictation  = false
+    var bvDictation     = false
+    var llyDictation    = false
+    var gjDictation     = false
+    var hDictation      = false
     
-    
+    //------------------------------------------------------
+    // MARK: - View did load
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(tildeDictation)
-        print(bvDictation)
         
+        // Initialization
         whiteBackground.alpha = 0
-        correctedWordUIView.alpha = 0
+        correctedWordHeightConstraint.constant = 0
         
         correctedWordTextField.delegate      = self
 
         // Add notification for keyboard heigth change
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardNotification(notification:)), name: UIResponder.keyboardWillChangeFrameNotification,object:nil)
         
-        
-        // Add tap gesture to dismiss keyboard when user tap on the view controller (out of the keyboard)
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tap)
-        
+        // Add tap gesture to modify a word
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapOnTextView(_:)))
         dictadoTextView.addGestureRecognizer(tapGesture)
         
-        let dictadoWithoutAccent = removeAcent(from: dictado)
+        // generates dictation
+        let dictation = writeDictation(from: originalText)
         
-        let dictadoWithoutAccentWithRandomBV = randomBV(in: dictadoWithoutAccent)
-        
-        let dictadoWithFormat = formatDictado(dictadoWithoutAccentWithRandomBV)
-        
-        dictadoTextView.attributedText = dictadoWithFormat
-        dictadoTextView.isEditable = false
-        dictadoTextView.isSelectable = false
-        
+        // shows dictation on textview
+        dictadoTextView.attributedText = formatDictado(dictation)
+        dictadoTextView.isEditable      = false
+        dictadoTextView.isSelectable    = false
     }
+    
+    
+    //------------------------------------------------------
+    // MARK: - remove notification observer when app finish
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
 
-    // user tapped on textView.
-    // Select word tapped and shows it on label
-    @objc private final func tapOnTextView(_ tapGesture: UITapGestureRecognizer){
-        
-        whiteBackground.alpha = 0.8
-        correctedWordUIView.alpha = 1
+    //------------------------------------------------------
+    // Gives format to dictado
+    func formatDictado(_ dictado: String) -> NSAttributedString {
+            
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 15
 
-        let point = tapGesture.location(in: dictadoTextView)
-        if let detectedWord = getWordAtPosition(point) {
-            correctedWordTextField.text = detectedWord
-        }
-    }
-    
-    ///////////////////////////////////////////
-    //MARK:- keyboard Notification
-    
-    @objc func keyboardNotification(notification: NSNotification) {
-        
-        if let userInfo = notification.userInfo {
-            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-            let endFrameY = endFrame!.origin.y
-            let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
-            if endFrameY >= UIScreen.main.bounds.size.height {
-                self.correctedWordHeightConstraint.constant = 50 //heigth of the message view when it's on the bottom side
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 18),
+            .paragraphStyle: paragraphStyle
+        ]
 
-            } else {
-                self.correctedWordHeightConstraint?.constant = endFrame?.size.height ?? 50
-            }
-            UIView.animate(withDuration: duration,
-                           delay: TimeInterval(0),
-                           options: .curveLinear,
-                           animations: { self.view.layoutIfNeeded() },
-                           completion: nil)
-        }
+        return NSAttributedString(string: dictado, attributes: attributes)
     }
     
+    //------------------------------------------------------
+    // MARK: - write dictation with options method
     
-    ////////////////////////////////////////////
-    //MARK:- dismiss keyboard
-    
-    // dismiss keyboard
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    
-    
-    // gets the word closest to user tapped point
-    private func getWordAtPosition(_ point: CGPoint) -> String?{
-    if let textPosition = dictadoTextView.closestPosition(to: point)
-    {
-        if let range = dictadoTextView.tokenizer.rangeEnclosingPosition(textPosition, with: .word, inDirection: UITextDirection(rawValue: 1))
-      {
-        return dictadoTextView.text(in: range)
-      }
-    }
-    return nil
+    private func writeDictation(from originalText: String) -> String {
         
+        var dictation = originalText
+        
+        if tildeDictation {
+            dictation = removeAcent(from: originalText)
+        }
+        
+        //let dictadoWithoutAccent = removeAcent(from: originalText)
+        
+        if bvDictation {
+            dictation = randomBV(in: dictation)
+        }
+        //let dictadoWithoutAccentWithRandomBV = randomBV(in: dictadoWithoutAccent)
+        
+        return dictation
     }
-    
     
     // returns a dictado without accents
     func removeAcent(from dictado : String) -> String {
-        
         return  dictado.folding(options: .diacriticInsensitive, locale: .current)
-        
     }
     
     // returns a dictado without "b" and "v" added randomly
@@ -156,32 +136,77 @@ class DictationViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+    //------------------------------------------------------
+    // MARK: - tap on textView event
+    // Selects the word tapped and shows it on label
     
-    
-    
-    // Gives format to dictado
-    func formatDictado(_ dictado: String) -> NSAttributedString {
-            
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = 15
-
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 18),
-            .paragraphStyle: paragraphStyle
-        ]
-
-        return NSAttributedString(string: dictado, attributes: attributes)
+    @objc private final func tapOnTextView(_ tapGesture: UITapGestureRecognizer){
         
+        // Blurs the dictation and shows the text field with the word tapped
+        whiteBackground.alpha       = 0.8
+        correctedWordTextField.becomeFirstResponder()
+        
+        let point = tapGesture.location(in: dictadoTextView)
+        if let detectedWord = getWordAtPosition(point) {
+            correctedWordTextField.text = detectedWord
+        }
+    }
+    
+    // gets the word closest to user tapped point
+    private func getWordAtPosition(_ point: CGPoint) -> String? {
+        if let textPosition = dictadoTextView.closestPosition(to: point) {
+            if let range = dictadoTextView.tokenizer.rangeEnclosingPosition(textPosition, with: .word, inDirection: UITextDirection(rawValue: 1)) {
+                return dictadoTextView.text(in: range)
+            }
+        }
+        return nil
+    }
+    
+    //------------------------------------------------------
+    //MARK:- keyboard will change frame notification
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+
+        if let userInfo = notification.userInfo {
+            if let keyboardSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue{
+                
+                let keyboardYPos = keyboardSize.origin.y
+                
+                if keyboardYPos < UIScreen.main.bounds.size.height {  // keyboard shown
+                    self.correctedWordHeightConstraint.constant = keyboardSize.size.height  + 50
+                } else {
+                    self.correctedWordHeightConstraint.constant = 0 //heigth of the message view when it's on the bottom side
+                }
+                
+                let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+                UIView.animate(withDuration: duration,
+                               delay: TimeInterval(0),
+                               options: .curveLinear,
+                               animations: { self.view.layoutIfNeeded() },
+                               completion: nil)
+            }
+        }
+    }
+    
+    
+    //------------------------------------------------------
+    //MARK:- dismiss keyboard
+    
+    private func dismissKeyboard(){
+        
+        whiteBackground.alpha       = 0
+        correctedWordTextField.resignFirstResponder()
+        view.endEditing(true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         dismissKeyboard()
-        return false
+        return true
     }
     
     @IBAction func okButtonPressed(_ sender: UIButton) {
-        view.endEditing(true)
-
+        AudioServicesPlaySystemSound(1156)
+        dismissKeyboard()
     }
 
 }
